bf19a15a6b1d1d168ff616eeeb2271e2310c336a
Dan Jost <danjjost@gmail.com>
2024-11-27 00:02:56 -0600

    Use percentages in result display

--- components/hooks/usePhotoEvaluation.ts
+++ components/hooks/usePhotoEvaluation.ts
@@ -1,4 +1,4 @@
-import Toast from "react-native-root-toast";
+import Toast, { ToastOptions } from "react-native-root-toast";
 import {
   EvaluatePhotoClient,
   EvaluationResults,
@@ -24,24 +24,66 @@ async function evaluatePhotoAndDisplayResults(photoBase64?: string) {
   return evaluationResults;
 }
 
+const getToastOptions = (
+  positionOffset: number,
+  value?: number
+): ToastOptions => ({
+  duration: 3000,
+  position: positionOffset,
+  shadow: true,
+  animation: true,
+  hideOnPress: false,
+  backgroundColor: colorFromValue(value ?? 0),
+});
+
+function getPosition(value: number) {
+  return Toast.positions.BOTTOM - 50 * value;
+}
+
+function getPredictionToastOptions(
+  positionOffset: number,
+  color: string
+): ToastOptions | undefined {
+  return {
+    duration: 3000,
+    position: positionOffset,
+    shadow: true,
+    animation: true,
+    hideOnPress: false,
+    backgroundColor: color,
+    textColor: "black",
+  };
+}
+
 function showResultsToast(results: EvaluationResults) {
   const prediction = getPrediction(results);
 
   Toast.show(
-    `
-          Prediction: ${prediction}\r\n
-          Captain: ${results.captain}\r\n 
-          B: ${results.bathroom_cat}\r\n, 
-          Control: ${results.control}`,
-    {
-      duration: 3000,
-      position: Toast.positions.BOTTOM,
-      shadow: true,
-      animation: true,
-      hideOnPress: true,
-      backgroundColor: "lightgreen",
-    }
+    "Prediction: " + prediction,
+    getPredictionToastOptions(getPosition(3), "lightblue")
   );
+
+  Toast.show(
+    `😻 ${toPercent(results.captain)}%`,
+    getToastOptions(getPosition(2), results.captain)
+  );
+
+  Toast.show(
+    `😼 ${toPercent(results.bathroom_cat)}%`,
+    getToastOptions(getPosition(1), results.bathroom_cat)
+  );
+
+  Toast.show(
+    `🪑 ${toPercent(results.control)}%`,
+    getToastOptions(getPosition(0), results.control)
+  );
+}
+
+function colorFromValue(value: number): string | undefined {
+  const red = Math.floor(255 * (1 - value));
+  const green = Math.floor(255 * value);
+
+  return `rgb(${red}, ${green}, 0)`;
 }
 
 function showEvaluatePhotoErrorToast() {
@@ -64,8 +106,12 @@ const getPrediction = (results: EvaluationResults) => {
     results.control ?? -1
   );
 
-  if (max === results.captain) return "😻 Captain";
-  if (max === results.bathroom_cat) return "😼 Bathroom Cat";
-  if (max === results.control) return "🪑 Control";
+  if (max === results.captain) return "😻";
+  if (max === results.bathroom_cat) return "😼";
+  if (max === results.control) return "🪑";
   return "No prediction available.";
 };
+
+function toPercent(captain: number | undefined) {
+  return captain ? Math.round(captain * 100) : 0;
+}
