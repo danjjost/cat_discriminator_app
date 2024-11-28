29ee317b2f3e8e64bbcb286b7203d95dba39d57c
Dan Jost <danjjost@gmail.com>
2024-11-27 18:27:26 -0600

    Refactor usePhotoEvaluation

--- components/hooks/usePhotoEvaluation.ts
+++ components/hooks/usePhotoEvaluation.ts
@@ -14,96 +14,82 @@ async function evaluatePhotoAndDisplayResults(photoBase64?: string) {
   );
 
   try {
-    if (evaluationResults) showResultsToast(evaluationResults);
-    else throw new Error("Failed to evaluate photo.");
+    if (evaluationResults) {
+      showResultsToasts(evaluationResults);
+    } else {
+      throw new Error("Failed to evaluate photo.");
+    }
   } catch (error) {
     console.error("Failed to take photo:", error);
-    showEvaluatePhotoErrorToast();
+    showPhotoEvaluationErrorToast();
   }
 
   return evaluationResults;
 }
 
+// ---- Toast Functions ----
+
+function showResultsToasts(results: EvaluationResults) {
+  const prediction = getPrediction(results);
+
+  showToast(0, "Prediction: " + prediction, "lightblue");
+  showPercentageToast(1, "😻", results.captain);
+  showPercentageToast(2, "😼", results.bathroom_cat);
+  showPercentageToast(3, "🪑", results.control);
+}
+
+const showToast = (
+  position: 3 | 2 | 1 | 0,
+  message: string,
+  colorOverride?: string
+) => Toast.show(message, getToastOptions(position, colorOverride));
+
+const showPercentageToast = (
+  position: 3 | 2 | 1 | 0,
+  title: string,
+  confidenceAsDecimalBetween0And1: number | undefined
+) =>
+  showToast(
+    position,
+    `${title} ${toPercent(confidenceAsDecimalBetween0And1)}%`,
+    getColorFromConfidence(confidenceAsDecimalBetween0And1 ?? 0)
+  );
+
+const showPhotoEvaluationErrorToast = () =>
+  Toast.show("❌ Failed to Evaluate Photo.", errorToastOptions);
+
+// ---- Toast Options ----
+
 const getToastOptions = (
-  positionOffset: number,
-  value?: number
+  position: 3 | 2 | 1 | 0,
+  color?: string
 ): ToastOptions => ({
+  position: getPositionOffset(position),
+  backgroundColor: color,
+  ...defaultToastOptions,
+});
+
+const defaultToastOptions = {
   duration: 3000,
-  position: positionOffset,
   shadow: true,
   animation: true,
   hideOnPress: false,
-  backgroundColor: colorFromValue(value ?? 0),
-});
+};
+const errorToastOptions = {
+  position: Toast.positions.TOP,
+  backgroundColor: "red",
+  ...defaultToastOptions,
+};
 
-function getPosition(value: number) {
-  return Toast.positions.BOTTOM - 50 * value;
-}
-
-function getPredictionToastOptions(
-  positionOffset: number,
-  color: string
-): ToastOptions | undefined {
-  return {
-    duration: 3000,
-    position: positionOffset,
-    shadow: true,
-    animation: true,
-    hideOnPress: false,
-    backgroundColor: color,
-    textColor: "black",
-  };
-}
-
-function showResultsToast(results: EvaluationResults) {
-  const prediction = getPrediction(results);
-
-  Toast.show(
-    "Prediction: " + prediction,
-    getPredictionToastOptions(getPosition(3), "lightblue")
-  );
-
-  Toast.show(
-    `😻 ${toPercent(results.captain)}%`,
-    getToastOptions(getPosition(2), results.captain)
-  );
-
-  Toast.show(
-    `😼 ${toPercent(results.bathroom_cat)}%`,
-    getToastOptions(getPosition(1), results.bathroom_cat)
-  );
-
-  Toast.show(
-    `🪑 ${toPercent(results.control)}%`,
-    getToastOptions(getPosition(0), results.control)
-  );
-}
-
-function colorFromValue(value: number): string | undefined {
-  const red = Math.floor(255 * (1 - value));
-  const green = Math.floor(255 * value);
-
-  return `rgb(${red}, ${green}, 0)`;
-}
-
-function showEvaluatePhotoErrorToast() {
-  Toast.show("❌ Failed to Evaluate Photo.", {
-    duration: 1500,
-    position: Toast.positions.BOTTOM,
-    shadow: true,
-    animation: true,
-    hideOnPress: true,
-    backgroundColor: "red",
-  });
-}
+// ---- Helper Functions ----
 
 const getPrediction = (results: EvaluationResults) => {
   if (!results) return "No prediction available.";
 
   const max = Math.max(
-    results.captain ?? -1,
-    results.bathroom_cat ?? -1,
-    results.control ?? -1
+    results.captain ?? 0,
+    results.bathroom_cat ?? 0,
+    results.control ?? 0
   );
 
   if (max === results.captain) return "😻";
@@ -112,6 +98,14 @@ const getPrediction = (results: EvaluationResults) => {
   return "No prediction available.";
 };
 
-function toPercent(captain: number | undefined) {
-  return captain ? Math.round(captain * 100) : 0;
-}
+const getColorFromConfidence = (value: number): string | undefined => {
+  const red = Math.floor(255 * (1 - value));
+  const green = Math.floor(255 * value);
+
+  return `rgb(${red}, ${green}, 0)`;
+};
+
+const toPercent = (captain: number | undefined) =>
+  captain ? Math.round(captain * 100) : 0;
+
+const getPositionOffset = (value: number) => Toast.positions.TOP + 50 * value;
